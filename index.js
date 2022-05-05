b60b1ad4481be8a15ea6131d95dd9ebb251f2264
Aryan Babber <86878236+AryanBabber@users.noreply.github.com>
2022-05-06 05:04:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let container = document.getElementById('container');
+let loginPage = document.getElementById('login-page')
+let registerPage = document.getElementById('register-page')
+
+loginPage.addEventListener('click', ()=>{
+    
+})
+
+registerPage.addEventListener('click', ()=>{
+    
+})
+
+
+// comments
+
+/* 
+1. Password showing hiding feature with an eye
+2. Both login and registered users will take the data in the local storage
+3. Both pages will hover over the text on the index.html
+
+
+*/
